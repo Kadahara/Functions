754ff53fac329785cf122d05069a8bfb0a0ad2d5
User <User@WIN-E8F88FA2UKH>
2023-02-10 07:59:17 +0300

    Фибоначи

--- Rekursion/Source.cpp
+++ Rekursion/Source.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 void elevator(int floor);
 int factorial(int n);
+int fib(int n);
 
 //#define ELEVATOR
 
@@ -16,9 +17,23 @@ void main()
 	cout << "Введите номер этажа: "; cin >> n;
 	elevator(n);
 #endif
+	int num, num_max;
 	int n;
-	cout << "Число которое хотите возвести в факториал: "; cin >> n;
-	cout << factorial(n);
+	cout << "Число которое хотите возвести в факториал: "; cin >> n; 
+	cout << "Кол во чисел из ряда фибоначи: "; cin >> num;
+	cout << "Предельное число из ряда фибоначи: "; cin >> num_max;
+	cout << factorial(n) << endl;
+	//cout << fib(num);
+	for (int i = 1; i <= num; i++)
+	{
+	cout << fib(i) << "\t";
+	}
+	cout << endl;
+	for (int i = 1; fib(i) <= num_max; i++)
+	{
+		cout << fib(i) << "\t";
+	}
+
  }
 void elevator(int floor)
 {
@@ -35,4 +50,16 @@ int factorial(int n)
 {
 	if (n == 0)return 1;
 	else return n * factorial(n - 1);
+}
+int fib(int num)
+{
+	if (num < 1)
+	{
+		return 0;
+	}
+	if (num == 1)
+	{
+		return 1;
+	}
+	return fib(num - 1) + fib(num - 2);
 }
